9f67fed132003d434cd90e14001bec6a30fdd0ca
Fran Bosquet <fran@redradix.com>
2018-05-25 13:06:07 +0200

    Prepare repo for Kata

--- src/lib.js
+++ src/lib.js
@@ -1,65 +1,12 @@
-function moveRight(input) {
-  return input.map(moveRowRight)
-}
+const rowDirections = ['left', 'right']
+const columnDirections = ['up', 'down']
 
-function moveLeft(input) {
-  return input.map(moveRowLeft)
-}
+const collapeseRows = (direction, board) => board
+const collapeseColumns = (direction, board) => board
 
-function collapse(list) {
-  if (list.length < 2) return list
-  if (list[0] === list[1]) {
-    return [list[0] + list[1], ...collapse(list.slice(2))]
+module.exports = (direction, board) => {
+  if (rowDirections.includes(direction)) {
+    return collapseRows(direction, board)
   }
-  return [list[0], ...collapse(list.slice(1))]
-}
-
-function notEmptySpot(spot) {
-  return spot !== 0
-}
-
-function padEmptySpotsRight(list) {
-  if (list.length === 4)
-    return list
-
-  return padEmptySpotsRight([...list, 0])
-}
-
-function moveRowLeft(input) {
-  return padEmptySpotsRight(collapse(input.filter(notEmptySpot)))
-}
-
-function moveRowRight(input) {
-  return moveRowLeft(input.reverse()).reverse()
-}
-
-function moveUp(input) {
-  return rotateCounterClockwise(moveRight(rotateClockwise(input)))
-}
-
-function moveDown(input) {
-  return rotateCounterClockwise(moveLeft(rotateClockwise(input)))
-}
-
-function rotateCounterClockwise(input) {
-  return rotateClockwise(rotateClockwise(rotateClockwise(input)))
-}
-
-function rotateClockwise(input) {
-  return input.reduce((output, row, i) => {
-    for(let j = 0; j<4; j++){
-      output[j][3-i] = row[j]
-    }
-    return output
-  }, [[],[],[],[]])
-}
-
-module.exports = {
-  moveRight,
-  moveRowLeft,
-  moveRowRight,
-  moveUp,
-  moveDown,
-  rotateClockwise,
-  rotateCounterClockwise
+  return collapseColumns(direction, board)
 }
--- src/test.js
+++ src/test.js
@@ -1,154 +1,27 @@
+const collapse = require('./lib')
 
-const {
-  moveRight,
-  moveRowLeft,
-  moveRowRight,
-  moveUp,
-  moveDown,
-  rotateClockwise,
-  rotateCounterClockwise
-} = require('./lib')
+const emptyBoard = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 
 describe('2048', () => {
-  describe('Move right', () => {
-    it('Moves single value right', () => {
-      const input = [0, 2, 0, 0]
-      const output = [0, 0, 0, 2]
-      expect(moveRowRight(input)).toEqual(output)
-    })
-
-    it('Moves two diferent values as right as they can', () => {
-      const input = [2, 0, 4, 0]
-      const output = [0, 0, 2, 4]
-      expect(moveRowRight(input)).toEqual(output)
-    })
-
-    it('Moves two equal values and adds them as right as they can', () => {
-      const input = [2, 0, 2, 0]
-      const output = [0, 0, 0, 4]
-      expect(moveRowRight(input)).toEqual(output)
-    })
-
-    it('Moves three equal values as right as they can, adding only the two righter ones', () => {
-      const input = [2, 2, 2, 0]
-      const output = [0, 0, 2, 4]
-      expect(moveRowRight(input)).toEqual(output)
-    })
-
-    it('Moves three equal values as right as they can, adding only the two righter ones', () => {
-      const input = [2, 2, 2, 2]
-      const output = [0, 0, 4, 4]
-      expect(moveRowRight(input)).toEqual(output)
-    })
-
-    it('Moves three equal values as right as they can, adding only the two righter ones', () => {
-      const input = [4, 2, 2, 0]
-      const output = [0, 0, 4, 4]
-      expect(moveRowRight(input)).toEqual(output)
-    })
-  })
-
-  describe('Move left', () => {
-    it('Moves single value left', () => {
-      const input = [0, 0, 2, 0]
-      const output = [2, 0, 0, 0]
-      expect(moveRowLeft(input)).toEqual(output)
-    })
-
-    it('Moves two diferent values as left as they can', () => {
-      const input = [0, 4, 0, 2]
-      const output = [4, 2, 0, 0]
-      expect(moveRowLeft(input)).toEqual(output)
-    })
-
-    it('Moves two equal values and adds them as left as they can', () => {
-      const input = [0, 2, 0, 2]
-      const output = [4, 0, 0, 0]
-      expect(moveRowLeft(input)).toEqual(output)
-    })
-
-    it('Moves three equal values as left as they can, adding only the two lefter ones', () => {
-      const input = [0, 2, 2, 2]
-      const output = [4, 2, 0, 0]
-      expect(moveRowLeft(input)).toEqual(output)
-    })
-
-    it('Moves three equal values as left as they can, adding only the two lefter ones', () => {
-      const input = [2, 2, 2, 2]
-      const output = [4, 4, 0, 0]
-      expect(moveRowLeft(input)).toEqual(output)
-    })
-
-    it('Moves three equal values as left as they can, adding only the two lefter ones', () => {
-      const input = [0, 4, 2, 2]
-      const output = [4, 4, 0, 0]
-      expect(moveRowLeft(input)).toEqual(output)
-    })
-  })
-
-  describe('Move up', () => {
-    const input = [
-      [0, 0, 2, 0], 
-      [2, 2, 2, 4], 
-      [0, 2, 2, 2], 
-      [2, 2, 2, 2]]
-    const output = [
-      [4, 4, 4, 4], 
-      [0, 2, 4, 4], 
-      [0, 0, 0, 0], 
-      [0, 0, 0, 0]]
-    
-    it('Moves up the entire board', () => {
-      expect(moveUp(input)).toEqual(output)
-    })
-  })
-
-  describe('Move down', () => {
-    const input = [
-      [0, 0, 2, 0],
-      [2, 2, 2, 4],
-      [0, 2, 2, 2],
-      [2, 2, 2, 2]]
-    const output = [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 2, 4, 4],
-      [4, 4, 4, 4]]
-
-    it('Moves up the entire board', () => {
-      expect(moveDown(input)).toEqual(output)
-    })
-  })
-
-  describe('Rotate clockwise', () => {
-    const input = [
-      [0, 0, 2, 0],
-      [2, 2, 2, 4],
-      [0, 2, 2, 2],
-      [2, 2, 2, 2]]
-    const output = [
-      [2, 0, 2, 0],
-      [2, 2, 2, 0],
-      [2, 2, 2, 2],
-      [2, 2, 4, 0]]
-    it('Rotates the array clockwise', () => {
-      expect(rotateClockwise(input)).toEqual(output)
-    })
-  })
-
-  describe('Rotate counter clockwise', () =>  {
-    const output = [
-      [0, 0, 2, 0],
-      [2, 2, 2, 4],
-      [0, 2, 2, 2],
-      [2, 2, 2, 2]]
-    const input = [
-      [2, 0, 2, 0],
-      [2, 2, 2, 0],
-      [2, 2, 2, 2],
-      [2, 2, 4, 0]]
-    it('Rotates the array counter clockwise', () => {
-      expect(rotateCounterClockwise(input)).toEqual(output)
+  describe('operating on a board with two 2 on the same row', () => {
+    it(`when collapsing right converts 
+    [0, 2, 2, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+    into
+    [0, 0, 0, 4],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]`, () => {
+      expect(
+        collapse('right', [
+          [0, 0, 2, 2],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]
+        ])
+      ).toEqual([[0, 0, 0, 4], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]])
     })
   })
 })
