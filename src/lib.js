bd9c73d2784bd6c815962e4aa62597d739d4fe02
Daniel Grijuela <danielgrijuela@gmail.com>
2018-05-25 16:46:19 +0200

    Implement move right and left functionality

--- src/lib.js
+++ src/lib.js
@@ -1,1 +1,34 @@
-module.exports = {}
+const moveMatrixLeft = matrix => matrix.map(moveRowLeft);
+const moveRowLeft = row => {
+  let newArray = [];
+  row.filter(el => el !== 0).forEach(el => {
+      if (newArray[newArray.length - 1] === el) {
+        newArray[newArray.length - 1] = el * 2;
+      } else {
+        newArray.push(el);
+      }
+  });
+  const missingZeros = row.length - newArray.length;
+  return newArray.concat(new Array(missingZeros).fill(0))
+};
+
+const moveMatrixRight = matrix => matrix.map(moveRowRight);
+const moveRowRight = row => {
+  let newArray = [];
+  row.reverse().filter(el => el !== 0).forEach(el => {
+      if (newArray[newArray.length - 1] === el) {
+        newArray[newArray.length - 1] = el * 2;
+      } else {
+        newArray.push(el);
+      }
+  });
+  const missingZeros = row.length - newArray.length;
+  return new Array(missingZeros).fill(0).concat(newArray.reverse());
+};
+
+module.exports = {
+  moveMatrixRight,
+  moveRowRight,
+  moveMatrixLeft,
+  moveRowLeft
+};
