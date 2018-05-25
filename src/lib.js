de667eef2c36e9d9280f84048561f82b39882c8e
alejandrodob <alejandrodob@gmail.com>
2018-05-25 17:40:40 +0200

    Check collapse condition with head and second values after movug zeros

--- src/lib.js
+++ src/lib.js
@@ -1,7 +1,7 @@
 const rowDirections = ['left', 'right']
 const columnDirections = ['up', 'down']
 
-const collapseRowToRight = (row) => {
+const collapseRowToRight = row => {
   return collapseRowToLeft(row.slice().reverse()).reverse()
 }
 
@@ -9,29 +9,41 @@ const rowCollapsed = row => row.length === 0 || row.length === 1
 const shouldMove = ([head]) => head === 0
 const shouldCollapse = ([head, second]) => head === second
 
-const collapseRowToLeft = (row) => {
+const collapseRowToLeft = row => {
   if (rowCollapsed(row)) {
     return row
   }
-  const [head, second, ...tail] = row.filter(x => x).concat([0, 0, 0, 0]).slice(0, row.length)
-  if (shouldCollapse(row)) {
+  const [head, second, ...tail] = row
+    .filter(x => x)
+    .concat([0, 0, 0, 0])
+    .slice(0, row.length)
+
+  if (shouldCollapse([head, second])) {
     return [head + second, ...collapseRowToLeft(tail), 0]
   }
   return [head, ...collapseRowToLeft([second, ...tail])]
 }
 
 const transpose = board =>
-  board.map(row => row.slice().reverse()).slice().reverse()
-
+  board
+    .map(row => row.slice().reverse())
+    .slice()
+    .reverse()
 
 const collapseRows = (direction, board) => {
-  return board.map(direction === 'left' ? collapseRowToLeft : collapseRowToRight)
+  return board.map(
+    direction === 'left' ? collapseRowToLeft : collapseRowToRight
+  )
 }
 
 const collapseColumns = (direction, board) => {
   const transposed = transpose(board)
-  return transposed.map(direction === 'up' ? collapseRowToLeft : collapseRowToRight)
+  return transposed.map(
+    direction === 'up' ? collapseRowToLeft : collapseRowToRight
+  )
 }
 
 module.exports = (direction, board) =>
-  rowDirections.includes(direction) ? collapseRows(direction, board) : collapseColumns(direction, board)
+  rowDirections.includes(direction)
+    ? collapseRows(direction, board)
+    : collapseColumns(direction, board)
